03bd352e99e729f7efb404e75e55d7b051743f6b
cemgthedev <cemg.the.dev@gmail.com>
2023-07-09 21:09:31 -0300

    aprendendo lógica de uso da renderização condicional para mostrar tela de loading, tela de erro e tela de sucesso

--- app/src/components/Table/index.tsx
+++ app/src/components/Table/index.tsx
@@ -1,52 +1,52 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import '../../styles/global.css'
+import "../../styles/global.css";
 
-import { getDate } from '../../functions/dateFormatter'
+import { getDate } from "../../functions/dateFormatter";
 
-import { PopUp, PopUpProps, RepositoryProps } from '../PopUp';
+import { PopUp, PopUpProps, RepositoryProps } from "../PopUp";
 
 const listColors = [
-    'bg-green-300',
-    'bg-green-500',
-    'bg-green-700',
-    'bg-sky-300',
-    'bg-sky-500',
-    'bg-sky-700',
-    'bg-red-300',
-    'bg-red-500',
-    'bg-red-700',
-    'bg-amber-300',
-    'bg-amber-500',
-    'bg-amber-700',
-    'bg-indigo-300',
-    'bg-indigo-500',
-    'bg-indigo-700',
-    'bg-fuchsia-300',
-    'bg-fuchsia-500',
-    'bg-fuchsia-700',
-    'bg-cyan-300',
-    'bg-cyan-500',
-    'bg-cyan-700'
-]
+  "bg-green-300",
+  "bg-green-500",
+  "bg-green-700",
+  "bg-sky-300",
+  "bg-sky-500",
+  "bg-sky-700",
+  "bg-red-300",
+  "bg-red-500",
+  "bg-red-700",
+  "bg-amber-300",
+  "bg-amber-500",
+  "bg-amber-700",
+  "bg-indigo-300",
+  "bg-indigo-500",
+  "bg-indigo-700",
+  "bg-fuchsia-300",
+  "bg-fuchsia-500",
+  "bg-fuchsia-700",
+  "bg-cyan-300",
+  "bg-cyan-500",
+  "bg-cyan-700",
+];
 
 export type TableProps = {
-    list: RepositoryProps[]
-}
+  list: RepositoryProps[];
+};
 
 export function Table({ ...props }: TableProps) {
-    let [openPopUp, setOpenPopUp] = useState<number | undefined>();
+  let [openPopUp, setOpenPopUp] = useState<number | undefined>();
 
-    const setOfTechnologies = new Set<string | null>();
-    props.list.map(item => {
-        setOfTechnologies.add(item.language);
-    })
+  const setOfTechnologies = new Set<string | null>();
+  props.list.map((item) => {
+    setOfTechnologies.add(item.language);
+  });
 
-    setOfTechnologies.delete(null);
+  setOfTechnologies.delete(null);
 
-    return (
-        <div 
-            className='font-semibold 
+  return (
+    <div
+      className="font-semibold 
                     text-white 
                       flex 
                       flex-col 
@@ -55,110 +55,105 @@ export function Table({ ...props }: TableProps) {
                       bg-slate-900 
                       w-[50vw] 
                       min-w-[320px] 
-                      rounded-md'
-        >
-            <div 
-                className='flex 
+                      rounded-md"
+    >
+      <div
+        className="flex 
                           items-center 
-                          gap-2'
-            >
-                <img src="/icons/BracketsCurly.svg" alt=""/>
-                <h1
-                    className='text-xl'
-                >
-                    Tecnologias
-                </h1>
-            </div>
-            <div 
-                className='flex 
+                          gap-2"
+      >
+        <img src="/icons/BracketsCurly.svg" alt="" />
+        <h1 className="text-xl">Tecnologias</h1>
+      </div>
+      <div
+        className="flex 
                           flex-wrap 
-                          gap-2'
-            >
-                {
-                    Array.from(setOfTechnologies).map(languageName => (
-                        <h1 
-                            key={languageName} 
-                            className={`select-none 
+                          gap-2"
+      >
+        {Array.from(setOfTechnologies).map((languageName) => (
+          <h1
+            key={languageName}
+            className={`select-none 
                                         flex 
                                         p-1 
                                         cursor-pointer 
                                         rounded-md 
                                         transition 
                                         hover:bg-opacity-60 
-                                        ${listColors[Math.floor(Math.random() * listColors.length)]}`} 
-                        >
-                            { languageName }
-                        </h1>
-                    ))
-                }
-            </div>
-            <div className='flex 
+                                        ${
+                                          listColors[
+                                            Math.floor(
+                                              Math.random() * listColors.length
+                                            )
+                                          ]
+                                        }`}
+          >
+            {languageName}
+          </h1>
+        ))}
+      </div>
+      <div
+        className="flex 
                             items-center 
-                            gap-2'
-            >
-                <img src="/icons/Folders.svg" alt=""/>
-                <h1
-                    className='text-xl'
-                >
-                    Repositórios
-                </h1>
-            </div>
-            <div 
-                className='bg-gray-50 
+                            gap-2"
+      >
+        <img src="/icons/Folders.svg" alt="" />
+        <h1 className="text-xl">Repositórios</h1>
+      </div>
+      <div
+        className="bg-gray-50 
                         text-gray-900 
                         break-words 
                         flex 
                         flex-col 
                         gap-2 
-                        rounded-md'
-            >
-                <div className='select-none 
+                        rounded-md"
+      >
+        <div
+          className="select-none 
                                flex 
                                justify-between 
                                p-2
                                border-b-2
-                               border-b-slate-900'
-                >
-                        <h1 
-                            className='w-1/3 
-                                       p-1'
-                        >
-                            Nome
-                        </h1>
-                        <h1 
-                            className='w-1/3 
+                               border-b-slate-900"
+        >
+          <h1
+            className="w-1/3 
+                                       p-1"
+          >
+            Nome
+          </h1>
+          <h1
+            className="w-1/3 
                                        text-center 
-                                       p-1'
-                        >
-                            Criação
-                        </h1>
-                        <h1 
-                            className='w-1/3 
+                                       p-1"
+          >
+            Criação
+          </h1>
+          <h1
+            className="w-1/3 
                                        text-center 
-                                       p-1'
-                        >
-                            Última Atualização
-                        </h1>
-                </div>
-                <div 
-                    id='table' 
-                    className='min-h-0 
+                                       p-1"
+          >
+            Última Atualização
+          </h1>
+        </div>
+        <div
+          id="table"
+          className="min-h-0 
                                max-h-[25vh] 
                                overflow-auto 
                                scrollbar-hide 
                                flex 
                                flex-col 
                                gap-2 
-                               p-2'
-                >
-                    {
-                        props.list.map((item) => (
-                            <div 
-                                key={ item.id }
-                            >
-                                <button
-                                    onClick={() => setOpenPopUp(item.id)} 
-                                    className='select-none 
+                               p-2"
+        >
+          {props.list.map((item) => (
+            <div key={item.id}>
+              <button
+                onClick={() => setOpenPopUp(item.id)}
+                className="select-none 
                                                cursor-pointer 
                                                flex 
                                                justify-between 
@@ -166,52 +161,48 @@ export function Table({ ...props }: TableProps) {
                                                ring-gray-900 
                                                transition 
                                                hover:bg-green-500 
-                                               w-full'
-                                >
-                                    <h1 
-                                        className='w-1/3 
+                                               w-full"
+              >
+                <h1
+                  className="w-1/3 
                                                    font-medium 
-                                                   p-1'
-                                    >
-                                        { item.name }
-                                    </h1>
-                                    <h1 
-                                        className='w-1/3 
-                                                   font-medium 
-                                                   flex 
-                                                   justify-center 
-                                                   items-center 
-                                                   p-1'
-                                    >
-                                        { getDate(item.created_at) }
-                                    </h1>
-                                    <h1 
-                                        className='w-1/3 
+                                                   p-1"
+                >
+                  {item.name}
+                </h1>
+                <h1
+                  className="w-1/3 
                                                    font-medium 
                                                    flex 
                                                    justify-center 
                                                    items-center 
-                                                   p-1'
-                                    >
-                                        { getDate(item.updated_at) }
-                                    </h1>
-                                </button>
-                                <PopUp 
-                                    {...
-                                        {
-                                            id : item.id,
-                                            openPopUp: openPopUp,
-                                            setOpenPopUp: setOpenPopUp,
-                                            repository: item
-                                        } as PopUpProps
-                                    }
-                                />
-                            </div>
-                        ))
-                    }
-                </div>
+                                                   p-1"
+                >
+                  {getDate(item.created_at)}
+                </h1>
+                <h1
+                  className="w-1/3 
+                                                   font-medium 
+                                                   flex 
+                                                   justify-center 
+                                                   items-center 
+                                                   p-1"
+                >
+                  {getDate(item.updated_at)}
+                </h1>
+              </button>
+              <PopUp
+                {...({
+                  id: item.id,
+                  openPopUp: openPopUp,
+                  setOpenPopUp: setOpenPopUp,
+                  repository: item,
+                } as PopUpProps)}
+              />
             </div>
+          ))}
         </div>
-    );
-    
+      </div>
+    </div>
+  );
 }
--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -1,119 +1,128 @@
-import { useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-import { RepositoryProps } from '../../components/PopUp';
-import { Header, HeaderProps } from '../../components/Header';
-import { Card, CardProps } from '../../components/Card';
-import { Table, TableProps } from '../../components/Table';
-import { List, ListProps, ListUsersProps } from '../../components/List';
-import { Footer, FooterProps } from '../../components/Footer'
-import { NotFound, NotFoundProps } from '../NotFound';
+import { RepositoryProps } from "../../components/PopUp";
+import { Header, HeaderProps } from "../../components/Header";
+import { Card, CardProps } from "../../components/Card";
+import { Table, TableProps } from "../../components/Table";
+import { List, ListProps, ListUsersProps } from "../../components/List";
+import { Footer, FooterProps } from "../../components/Footer";
+import { NotFound, NotFoundProps } from "../NotFound";
 
-import '../../styles/global.css';
+import "../../styles/global.css";
 
-type UserProps = { repos_url: string, followers_url: string } & HeaderProps & CardProps | null;
+type UserProps =
+  | ({ repos_url: string; followers_url: string } & HeaderProps & CardProps)
+  | null;
 
 export function Home() {
-    const {userNameHome, userNameResearched} = useParams<string>();
-    const [user, setUser] = useState<UserProps>(null);
-    const [repos, setRepos] = useState<RepositoryProps[]>([] as RepositoryProps[]);
-    const [followers, setFollowers] = useState([] as ListUsersProps[]);
-    
-    type TypePromise = UserProps | RepositoryProps[] | ListUsersProps[];
+  const { userNameHome, userNameResearched } = useParams<string>();
+  const [user, setUser] = useState<UserProps>(null);
+  const [repos, setRepos] = useState<RepositoryProps[]>(
+    [] as RepositoryProps[]
+  );
+  const [followers, setFollowers] = useState([] as ListUsersProps[]);
+  const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        function getData(url: string):Promise<TypePromise> {
+  type TypePromise = UserProps | RepositoryProps[] | ListUsersProps[];
 
-            const promise:Promise<TypePromise> = new Promise(async (resolve, reject) => {
-                try {
-                    const response = await axios.get(url);
-                    const data = response.data;
-                
-                    resolve(data);
-                } catch(error) {
-                    reject(error);
-                }
-            });
+  useEffect(() => {
+    function getData(url: string): Promise<TypePromise> {
+      const promise: Promise<TypePromise> = new Promise(
+        async (resolve, reject) => {
+          try {
+            const response = await axios.get(url);
+            const data = response.data;
 
-            return promise;
-
+            resolve(data);
+          } catch (error) {
+            reject(error);
+          }
         }
-        
-        const url: string = `https://api.github.com/users/${userNameResearched}`;
-        
-        (getData(url) as Promise<UserProps>).then(user => {
-            setUser(user); 
-            if(user){
-                return Promise.all([
-                    (getData(user.repos_url) as Promise<RepositoryProps[]>).then(repos => setRepos(repos)),
-                    (getData(user.followers_url) as Promise<ListUsersProps[]>).then(followers => setFollowers(followers))
-                ]);
-            }
-        }).catch(error => console.error(error));
+      );
 
-    }, []);
-    
-    if(user != null) {
-        return (
-            <div 
-                id='top'
-                className='flex 
+      return promise;
+    }
+
+    const url: string = `https://api.github.com/users/${userNameResearched}`;
+
+    (getData(url) as Promise<UserProps>)
+      .then(async (user) => {
+        setUser(user);
+        if (user) {
+          return Promise.all([
+            (getData(user.repos_url) as Promise<RepositoryProps[]>).then(
+              (repos) => setRepos(repos)
+            ),
+            (getData(user.followers_url) as Promise<ListUsersProps[]>).then(
+              (followers) => {
+                setFollowers(followers);
+                setLoading(false); // para de carregar após obter todos os dados
+              }
+            ),
+          ]);
+        }
+      })
+      .catch((error) => {
+        setLoading(false); // para de carregar se houver falha
+        console.error(error);
+      });
+  }, []);
+
+  return loading ? (
+    <h1>Carregando</h1>
+  ) : user ? (
+    <div
+      id="top"
+      className="flex 
                            flex-col 
                            items-center 
                            gap-4
-                           bg-white'
-            >
-                <Header 
-                    {...
-                        {
-                            login: userNameHome,
-                            location: user.location,
-                            html_url: user.html_url,
-                            email: user.email,
-                            twitter_username: user.twitter_username
-                        } as HeaderProps
-                    }
-                />
-                <main 
-                    className='flex 
+                           bg-white"
+    >
+      <Header
+        {...({
+          login: userNameHome,
+          location: user.location,
+          html_url: user.html_url,
+          email: user.email,
+          twitter_username: user.twitter_username,
+        } as HeaderProps)}
+      />
+      <main
+        className="flex 
                                flex-col 
-                               gap-4'
-                >
-                    <Card 
-                        {...
-                            {
-                                name: user.name,
-                                bio: user.bio,
-                                avatar_url: user.avatar_url,
-                                created_at: user.created_at,
-                                updated_at: user.updated_at,
-                                public_repos: user.public_repos,
-                                followers: user.followers,
-                                following: user.following
-                            } as CardProps
-                        }
-                    />
-                    <Table 
-                        {...
-                            {
-                                list: repos
-                            } as TableProps
-                        }
-                    />
-                    <List 
-                        {...
-                            {
-                                login: userNameHome,
-                                list: followers
-                            } as ListProps
-                        }
-                    />
-                </main>
-                <a 
-                    href='#top' 
-                    title='voltar para o topo da página' 
-                    className="animate-bounce 
+                               gap-4"
+      >
+        <Card
+          {...({
+            name: user.name,
+            bio: user.bio,
+            avatar_url: user.avatar_url,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+            public_repos: user.public_repos,
+            followers: user.followers,
+            following: user.following,
+          } as CardProps)}
+        />
+        <Table
+          {...({
+            list: repos,
+          } as TableProps)}
+        />
+        <List
+          {...({
+            login: userNameHome,
+            list: followers,
+          } as ListProps)}
+        />
+      </main>
+      <a
+        href="#top"
+        title="voltar para o topo da página"
+        className="animate-bounce 
                                font-bold 
                                text-2xl 
                                bg-slate-900 
@@ -129,33 +138,28 @@ export function Home() {
                                rounded-full
                                ring-2
                                ring-white"
-                >
-                    ^
-                </a>
-                <Footer
-                    {...
-                        {
-                            hrefInstagram: "https://www.instagram.com/cemg.the.dev/",
-                            hrefLinkedin: "https://www.linkedin.com/in/carlos-eduardo-moura-gomes-bb0ab7250/",
-                            hrefEmail: "mailto:cemg.the.dev@gmail.com",
-                            projectName: 'GitHub Stalker',
-                            licenceLabel: 'Copyright (c) 2023 Carlos Eduardo de Moura Gomes',
-                            hrefLicence: "https://github.com/cemgthedev/GitHub-Stalker/blob/main/LICENSE"
-                        } as FooterProps
-                    }
-                />
-            </div>
-        );
-    } else {
-        return (
-            <NotFound
-                {...
-                    {
-                        message: `Usuário não encontrado`,
-                        textButton: 'Voltar a página de pesquisa'
-                    } as NotFoundProps
-                }
-            />
-        );
-    }
+      >
+        ^
+      </a>
+      <Footer
+        {...({
+          hrefInstagram: "https://www.instagram.com/cemg.the.dev/",
+          hrefLinkedin:
+            "https://www.linkedin.com/in/carlos-eduardo-moura-gomes-bb0ab7250/",
+          hrefEmail: "mailto:cemg.the.dev@gmail.com",
+          projectName: "GitHub Stalker",
+          licenceLabel: "Copyright (c) 2023 Carlos Eduardo de Moura Gomes",
+          hrefLicence:
+            "https://github.com/cemgthedev/GitHub-Stalker/blob/main/LICENSE",
+        } as FooterProps)}
+      />
+    </div>
+  ) : (
+    <NotFound
+      {...({
+        message: `Usuário não encontrado`,
+        textButton: "Voltar a página de pesquisa",
+      } as NotFoundProps)}
+    />
+  );
 }
